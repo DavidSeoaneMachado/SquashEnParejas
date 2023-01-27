1ebef7dfcf5e833b8434afa7068c1a0177a31649
DavidSeoaneMachado <dseoanemachado@danielcastelao.org>
2023-01-27 13:00:29 +0100

    Squash

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("A");
         System.out.println("B");
+        //COLAB 1
+        //COLAB 2
+        //COLAB 3
+
     }
 }
